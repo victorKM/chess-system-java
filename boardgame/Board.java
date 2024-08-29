bdfc8c2342e8fae53c968d32ea69f2c6926bbd49
Victor Kikuti <victor.kikuti@gmail.com>
2024-08-29 16:20:16 -0300

    add pieces movement

--- boardgame/Board.java
+++ boardgame/Board.java
@@ -52,6 +52,22 @@ public class Board {
       piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+       if(!positionExists(position)) {
+          throw new BoardException("Position not on the board");
+       }
+       
+       if (piece(position) == null) {
+          return null;
+       } 
+
+       Piece aux = piece(position);
+       aux.position = null;
+       pieces[position.getRow()][position.getColumn()] = null;
+
+       return aux;
+    }
+
     private boolean positionExists(Integer row, Integer column) {
       return row >= 0 && row < rows && column >= 0 && column < columns;
     }
@@ -67,4 +83,6 @@ public class Board {
 
       return piece(position) != null;
     }
+
+
 }
